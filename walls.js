a0854209e8fb21e55a2d35a1680d57810e81dadd
Lena Krause <lena.krause@umontreal.ca>
2024-09-28 02:25:32 -0400

    trying stuff with walls

--- walls.js
+++ walls.js
@@ -3,22 +3,26 @@ var barWidth
 
 
 function setupWalls(){
+    background('black')
     angle = atan(windowWidth / windowHeight)
-
+    shapeSpeed = 100
     barWidth = 40
     diff = 25
 
 }
 
 
-function drawWalls(){
+function drawWalls(spectrum){
 
-
-    fill('white')
-    var nbStripes = (windowWidth/2) / (barWidth*2)
+    /*locationX = windowWidth * noise(shapeSpeed * frameCount) 
+    locationY = height * noise(shapeSpeed * frameCount)
+    */
+    //translate(locationX, locationY)
+    
+    var nbStripes = spectrum.length
     for (var i = 1; i <= nbStripes; i ++){
         console.log(i)
-        var x1 = barWidth*i*2
+        var x1 = barWidth*i*2 
         var y1 = (barWidth-diff)*i*2
     
         var height = windowHeight-(x1*2)
@@ -28,6 +32,7 @@ function drawWalls(){
             
             beginShape()
                 vertex(0, 0)
+                fill(spectrum[i], 200, 100)
                 vertex(barWidth, barWidth)
                 vertex(barWidth, height-barWidth)
                 vertex(0, height)
